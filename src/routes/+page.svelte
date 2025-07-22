5e769adf9f2045a240da1457bd15f4f2bd87410c
Kücüktofekci Tumucin Cagri <dr.tumucincagri@gmail.com>
2025-07-22 18:12:04 +0200

    hmm

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Userl from '$lib/userI.svelte'
+</script>
+
+<Userl />
--- src/routes/[id]/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import { page } from "$app/state";
-
-    const id = page.params.id
-</script>
-
-<h1>{id} szia</h1>
