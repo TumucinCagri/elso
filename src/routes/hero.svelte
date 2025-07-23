c68c9c8f487e200cf04a87376594ac4c37ab9b37
Kücüktofekci Tumucin Cagri <dr.tumucincagri@gmail.com>
2025-07-23 10:14:29 +0200

    szia

---
+++ src/routes/hero.svelte
@@ -0,0 +1,70 @@
+<script>
+    let name = 'top1'
+</script>
+
+<div style="background-image: url('/{name}.jpg');" class="hero">
+    <div class="box">
+        <h1>Welcome to XQ7T Site</h1>
+        <p></p>
+        <button>Join</button>
+        <input type="text" bind:value={name}>
+    </div>
+</div>
+
+<style>
+    .hero {
+        width: 100%;
+        height: 100vh;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+        text-align: center;
+        
+    }
+
+    .box {
+        backdrop-filter: blur(8px);
+        background-color: rgba(255, 255, 255, 0.2);
+        width: 40%;  
+        height: 40%;
+        border-radius: 20px;
+        padding: 15px;
+        gap: 15px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    h1 {
+        font-size: 55px;
+        text-shadow: 3px 3px 6px rgba(0, 0, 0, 0.4);
+        color: #222;
+    }
+
+    p {
+        text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.3);
+        color: #444;
+    }
+
+    button {
+        background-color: #444;       /* Szép kék alap */
+        color: white;                   /* Fehér szöveg */
+        padding: 12px 24px;             /* Kényelmes belső margó */
+        border: none;                  /* Nincs keret */
+        border-radius: 6px;            /* Lekerekített sarkok */
+        font-size: 16px;               /* Olvasható méret */
+        cursor: pointer;               /* Kéz ikon az egér fölött */
+        box-shadow: 0 4px 6px rgba(0,0,0,0.2); /* Enyhe árnyék */
+        transition: background-color 0.3s ease, box-shadow 0.3s ease; /* Szép átmenet */
+    }
+
+    button:hover {
+        background-color: #222;
+    }
+    </style>
---
+++ src/routes/load.svelte
@@ -0,0 +1,33 @@
+<script>
+    const ans = ['alma', 'szilva', 'apple']
+</script>
+
+<div class="main">
+    {#each ans as an}
+        <div class="box">{an}</div>
+    {/each}
+</div>
+
+<style>
+    .main {
+        width: 100%;
+        height: 100vh;
+        background-image: url('/top.jpg');
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 25px;
+    }
+
+    .box {
+        backdrop-filter: blur(14px);
+        border-radius: 15px;
+        width: 250px;
+        height: 350px;
+        transition: all 0.4s ease;
+    }
+
+    .box:hover {
+        transform: scale(1.1);
+    }
+</style>
